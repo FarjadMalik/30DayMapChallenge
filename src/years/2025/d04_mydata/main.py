e9a2bc45282e8159cd0340882e1b595c967b6f3f
Farjad Malik <m.farjad.malik@gmail.com>
2025-11-07 00:36:14 +0500

    my routes and pak floods maps

--- src/years/2025/d04_mydata/main.py
+++ src/years/2025/d04_mydata/main.py
@@ -3,8 +3,6 @@ import json
 import geopandas as gpd
 
 from pathlib import Path
-from shapely.geometry import box
-from rasterio.plot import reshape_as_image
 from src.utils.logger import get_logger
 from src.utils.helpers import get_relative_path
 
@@ -18,21 +16,32 @@ def load_routes(routes_json: dict) -> list[dict]:
     routes = routes_json.get("trips", [])
     trips = []
     for r in routes:
-        visit = r.get("place_visits", [])
-        # Expect exactly two place visits (source and destination)
+        visit = r.get("place_visit", [])
+        # Expect exactly two place visits (source and destination) with lat/lng
         if len(visit) < 2:
+            continue
+        if "place" not in visit[0] or "place" not in visit[1]:
             continue
         src = visit[0]["place"]["lat_lng"]
         dst = visit[1]["place"]["lat_lng"]
         
+        # transition can be walking, driving, etc.
+        mode = "unknown"
+        transition = r.get("transition", [])
+        if transition and isinstance(transition, list):
+            transition = transition[0]
+            route = transition.get("route", {})
+            if route:
+                mode = route.get("travel_mode", "unknown")
+    
         trip = {
             "id": r.get("id"),
-            "source": (src["latitude"], src["longitude"]),
-            "destination": (dst["latitude"], dst["longitude"]),
-            "transportation_mode": r.get("transition", "unknown")
+            "src": (src["latitude"], src["longitude"]),
+            "dst": (dst["latitude"], dst["longitude"]),
+            "t_mode": mode
         }
         trips.append(trip)
-
+    logger.info(f"Loaded {len(trips)} trips.")
     return trips
 
 def create_routes_map(path_dir: str, file_html: str):
@@ -45,22 +54,37 @@ def create_routes_map(path_dir: str, file_html: str):
     # Read the commute routes json file
     routes_path = "data/Takeout/Maps/Commute routes/Commute routes.json"
     routes_json = json.load(open(routes_path))
-    logger.info(f"Routes type: {type(routes_json)}")
     trips = load_routes(routes_json)
     logger.info(f"Trips: {trips}")
 
+    # Calculate center for the map
+    all_lat = [lat for tr in trips for lat,_ in (tr["src"], tr["dst"])]
+    all_lon = [lon for tr in trips for _,lon in (tr["src"], tr["dst"])]
+    map_center = (sum(all_lat)/len(all_lat), sum(all_lon)/len(all_lon))
+    
+    # Create the Folium map, centered on Pakistan (admin boundaries center)
+    basemap = folium.Map(location=map_center, zoom_start=10, tiles='OpenStreetMap')
 
-    # # Create the Folium map, centered on Pakistan (admin boundaries center)
-    # basemap = folium.Map(location=[center_lat, center_lon], zoom_start=6, tiles='OpenStreetMap')
+    # Plot the routes on a map    
+    for tr in trips:
+        folium.PolyLine(
+            locations=[ tr["src"], tr["dst"] ],
+            color="blue",
+            weight=5,
+            opacity=0.8,
+            popup=f'Trip Id: {tr["id"]}, Transportation Mode: {tr["t_mode"]}'
+        ).add_to(basemap)
+        # optionally: mark source & destination
+        folium.Marker(tr["src"], tooltip="Start", icon=folium.Icon(color="green")).add_to(basemap)
+        folium.Marker(tr["dst"], tooltip="End", icon=folium.Icon(color="red")).add_to(basemap)
 
-
-    # # Allows toggling between layers interactively 
-    # folium.LayerControl().add_to(basemap)
-    # # Save the map to an HTML file
-    # basemap.save(f"{Path(path_dir).parent}/{file_html}.html")
-    # logger.info(f"Map created – open '{file_html}.html' to view.")
+    # Allows toggling between layers interactively 
+    folium.LayerControl().add_to(basemap)
+    # Save the map to an HTML file
+    basemap.save(f"{Path(path_dir).parent}/{file_html}.html")
+    logger.info(f"Map created – open '{file_html}.html' to view.")
 
 
 if __name__ == "__main__":
-    out_filename = 'Leuven_walking_path'
+    out_filename = 'Google_routes_map'
     create_routes_map(path_dir=str(get_relative_path(__file__)), file_html=out_filename)
--- src/years/2025/d05_earth/main.py
+++ src/years/2025/d05_earth/main.py
@@ -12,20 +12,31 @@ from src.utils.helpers import get_relative_path
 logger = get_logger(__name__)
 
 
-def create_polygon_map(path_dir: str, file_html: str):
+def create_earth_map(path_dir: str, file_html: str):
     """
     Creates polygon map for Day 3 exercises
     
     """
     logger.info(f"Hello from {path_dir}")
+
+    # Geodatabase path
+    gdb_path = "data/PAK_misc/FL20250818PAK.gdb"
+    flood_layerlist = fiona.listlayers(gdb_path)
+    # logger.info(f"Layers in GDB: {flood_layerlist}")
+
+    # Load the flood extent polygon layer
+    flood_gdf = gpd.read_file(gdb_path, layer='VIIRS_20250826_20250907_FloodWaterExtent_PAK')
+    # Convert date to string for tooltip display
+    flood_gdf["Sensor_Date"] = flood_gdf["Sensor_Date"].astype(str)
+
+    
+    # Ensure the CRS is WGS84 (EPSG:4326) so it works with Folium
+    if flood_gdf is not None and flood_gdf.crs.to_string() != "EPSG:4326":
+        flood_gdf = flood_gdf.to_crs(epsg=4326)
     
     # Load the shapefile for pakistan admin boundaries
     shapefile_path = "data/pakistan_admin/gadm41_PAK_3.shp"
     admin_gdf = gpd.read_file(shapefile_path)
-
-    # # Ensure the CRS is WGS84 (EPSG:4326) so it works with Folium
-    # if admin_gdf is not None and admin_gdf.crs.to_string() != "EPSG:4326":
-    #     admin_gdf = admin_gdf.to_crs(epsg=4326)
 
     # Calculate a center for the map, e.g., the mean of the bounds
     bounds = admin_gdf.total_bounds  # [minx, miny, maxx, maxy]
@@ -35,6 +46,47 @@ def create_polygon_map(path_dir: str, file_html: str):
     # Create the Folium map, centered on Pakistan (admin boundaries center)
     basemap = folium.Map(location=[center_lat, center_lon], zoom_start=6, tiles='OpenStreetMap')
 
+    # TODO: join with admin boundaries to get names and areas for each admin unit
+    # Add flood layer to map
+    folium.GeoJson(
+        flood_gdf,
+        name='Flood Extents 2025',
+        style_function=lambda feature: {
+            'fillColor': 'blue',
+            'color': 'blue',
+            'weight': 1,
+        },
+        tooltip=folium.GeoJsonTooltip(
+            fields=['Area_ha', 'Area_m2', 'Sensor_ID', 'Sensor_Date'], 
+            aliases=['Area in Hectares', 'Area in meters', 'Sensor', 'Date'],
+            localize=True
+            )
+    ).add_to(basemap)
+
+    # Add title — can be done via Html in map
+    title_html = f"""
+         <h3 align="center" style="font-size:20px"><b>Floods Pakistan 2025</b></h3>
+         """
+    basemap.get_root().html.add_child(folium.Element(title_html))
+
+    # # Add Legend - Build HTML string
+    # items = "".join(
+    #     f"<i style='background:{colour};width:18px;height:18px;display:inline-block;margin-right:8px;'></i>{label}<br>"
+    #     for label, colour in legend_dict.items()
+    # )
+    # html = f"""
+    # <div style="position: fixed; { 'bottom: 50px;' if position.endswith('right') else '' } 
+    #              { 'right: 50px;' if position.endswith('right') else 'left:50px;' } 
+    #              width: 200px; height: auto; 
+    #              border:2px solid grey; z-index:9999; font-size:14px;
+    #              background-color:white; opacity:0.85; padding: 10px;">
+    #   <b>{title}</b><br>
+    #   {items}
+    # </div>
+    # """
+    # legend = branca.element.MacroElement()
+    # legend._template = branca.element.Template(html)
+    # map_obj.get_root().add_child(legend)
 
     # Allows toggling between layers interactively 
     folium.LayerControl().add_to(basemap)
@@ -45,4 +97,4 @@ def create_polygon_map(path_dir: str, file_html: str):
 
 if __name__ == "__main__":
     out_filename = 'flood_pakistan_2025'
-    create_polygon_map(path_dir=str(get_relative_path(__file__)), file_html=out_filename)
+    create_earth_map(path_dir=str(get_relative_path(__file__)), file_html=out_filename)
