308a5ce56de85b0b9bce8e611710f684108f0227
Farjad Malik <m.farjad.malik@gmail.com>
2025-11-16 22:39:27 +0500

    Add: d17 new tool

--- src/years/2025/d17_newtool/main.py
+++ src/years/2025/d17_newtool/main.py
@@ -1,7 +1,9 @@
-import folium
-import geopandas as gpd
+import pyvista as pv
 
 from pathlib import Path
+from pyvista import examples
+from pypalettes import load_cmap
+
 from src.utils.logger import get_logger
 from src.utils.helpers import get_relative_path
 
@@ -9,63 +11,36 @@ from src.utils.helpers import get_relative_path
 logger = get_logger(__name__)
 
 
-def generate_map(path_dir: str, file_html: str):
+def generate_map(path_dir: str, file_out: str):
     """
     Creates polygon map for Day 3 exercises
     
     """
-    logger.info(f"Hello from {path_dir}")
+    logger.info(f"Generating {path_dir}")
+
+    # Load and prepare the Earth topography data
+    land = examples.download_topo_land().triangulate().decimate(0.98)
+    land.point_data["Elevation"] = land.points[:, 2]
     
-    # Load the shapefile for pakistan admin boundaries
-    shapefile_path = "data/pakistan_admin/gadm41_PAK_3.shp"
-    admin_gdf = gpd.read_file(shapefile_path)
+    # Initialize the plotter with transparent background
+    p = pv.Plotter()
+    cmap = load_cmap("Coconut", cmap_type="continuous")
+    p.add_mesh(land, cmap=cmap, show_scalar_bar=False)  # Disable scalar bar
 
-    # Ensure the CRS is WGS84 (EPSG:4326) so it works with Folium
-    if admin_gdf is not None and admin_gdf.crs.to_string() != "EPSG:4326":
-        admin_gdf = admin_gdf.to_crs(epsg=4326)
+    # Set the background to be fully transparent
+    # p.background_color = (245, 40, 145, 0)
+    p.set_background(color="#080c14")
 
-    # Calculate a center for the map, e.g., the mean of the bounds
-    bounds = admin_gdf.total_bounds  # [minx, miny, maxx, maxy]
-    center_lat = (bounds[1] + bounds[3]) / 2
-    center_lon = (bounds[0] + bounds[2]) / 2
-    logger.info(f"Center Lat Lon: {center_lat, center_lon}")
+    # Save as image
+    output_path = Path(path_dir).parent / f"{file_out}"
+    # Export the plot to an HTML file with transparency
+    p.save_graphic(f"{output_path}.svg")
+    p.export_html(f"{output_path}.html")
     
-    # Create and Center a base map
-    basemap = folium.Map(location=[center_lat, center_lon], zoom_start=10, tiles='OpenStreetMap')
-
-    # Add desired data
-
-    # Allows toggling between layers interactively 
-    folium.LayerControl().add_to(basemap)
-    # Save the map to an HTML file
-    basemap.save(f"{Path(path_dir).parent}/{file_html}.html")
-    logger.info(f"Map created – open '{file_html}.html' to view.")
+    logger.info(f"Map created – open '{file_out}.png' to view.")
 
 
 if __name__ == "__main__":
-    # use pretty maps or a new tool as from JosephBARBIERDARNAL
+    # use pretty maps or a new tool pyvista, TODO: see and load other example data for this
     out_filename = 'newtool'
-    generate_map(path_dir=str(get_relative_path(__file__)), file_html=out_filename)
-
-
-#### 
-# import pyvista as pv
-# from pyvista import examples
-# from pypalettes import load_cmap
-
-# # Load and prepare the Earth topography data
-# land = examples.download_topo_land().triangulate().decimate(0.98)
-# land.point_data["Elevation"] = land.points[:, 2]
-
-# # Initialize the plotter with transparent background
-# p = pv.Plotter()
-# cmap = load_cmap("Coconut", cmap_type="continuous")
-# p.add_mesh(land, cmap=cmap, show_scalar_bar=False)  # Disable scalar bar
-
-# # Set the background to be fully transparent
-# # p.background_color = (245, 40, 145, 0)
-# p.set_background(color="#080c14")
-
-# # Export the plot to an HTML file with transparency
-# # p.export_html(".html")
-# # p.save_graphic(".svg")
+    generate_map(path_dir=str(get_relative_path(__file__)), file_out=out_filename)
